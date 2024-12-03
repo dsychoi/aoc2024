d5cf0be21c202a65438c1f6ce7f1ff52cad20491
dsychoi <dsychoi@gmail.com>
2024-12-02 23:21:54 -0600

    day 3

--- day3.py
+++ day3.py
@@ -1,25 +1,51 @@
-from urllib import request
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-url = 'https://adventofcode.com/2024/day/3/input'
-token = os.getenv('aoc_token')
-req = request.Request(
-    url, headers={'Cookie': f'session={token}'}
-)
-
-response = request.urlopen(req)
-raw_lines = response.read().decode('utf-8').strip().splitlines()
+from utils import fetch_data
+import re
 
 
 def parse_data(data):
-    parsed_data = []
+    result = []
     for line in data:
-        split_line = line.split()
-        parsed_data.append(split_line)
-    return parsed_data
+        result.append(line)
+    joined_result = "".join(result)
+    return joined_result
 
 
-print(parse_data(raw_lines))
+raw_data = fetch_data(3)
+parsed_data = parse_data(raw_data)
+
+
+def scan_corrupted_memory(memory):
+    pattern = r"mul\(\d+,\d+\)"
+    total = 0
+
+    matches = re.findall(pattern, memory)
+    for match in matches:
+        numbers = match.split('(')[1].split(')')[0]
+        left_number, right_number = numbers.split(',')
+        product = int(left_number) * int(right_number)
+        total += product
+    return total
+
+
+def scan_conditional_corrupted_memory(memory):
+    pattern = r"mul\(\d+,\d+\)|don't\(\)|do\(\)"
+    total = 0
+    skip = False
+
+    matches = re.findall(pattern, memory)
+    for match in matches:
+        if match.startswith("mul"):
+            numbers = match.split('(')[1].split(')')[0]
+            left_number, right_number = numbers.split(',')
+            product = int(left_number) * int(right_number)
+            if not skip:
+                total += product
+        elif match == "don't()":
+            skip = True
+        elif match == "do()":
+            skip = False
+    return total
+
+
+print(scan_corrupted_memory(parsed_data))
+print(scan_conditional_corrupted_memory(parsed_data))
